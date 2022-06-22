aef3f0bb6702ab9e030976a89602d9868ed74ae1
Alexander Shilen <aps2163@columbia.edu>
2022-06-22 14:38:37 -0400

    separate language server repo

--- client/src/utils/string.ts
+++
@@ -1,4 +0,0 @@
-const clearAndUpper = (text: string) => text.replace(/-/, "").toUpperCase();
-
-export const toCamelCase = (text: string) => text.replace(/-\w/g, clearAndUpper);
-export const toPascalCase = (text: string) => text.replace(/(^\w|-\w)/g, clearAndUpper);
